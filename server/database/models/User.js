23a616ca4afc2bc5e52d56a6e4cac427bf1a25d8
kanemototomoki <kanemototomoki@gmail.com>
2020-09-01 15:13:03 +0900

    fix: indent

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -1,6 +1,6 @@
 const { Model } = require('sequelize');
 
-module.exports= (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       User.hasMany(models.Todo, {
